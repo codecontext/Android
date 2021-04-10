f2f69d479683c01d87b9c0877d4b82539a3ae31d
codecontext <kri.i7teen@gmail.com>
2021-04-10 17:17:18 +0800

    Added toggle button functionalities for requesting device permissions
    Added onClick listener for toggle button, which requests for device media write and audio record permission.

--- SurfaceRecorder/app/src/main/java/com/example/surfacerecorder/MainActivity.java
+++ SurfaceRecorder/app/src/main/java/com/example/surfacerecorder/MainActivity.java
@@ -1,11 +1,24 @@
 package com.example.surfacerecorder;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import org.w3c.dom.Text;
 
@@ -15,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
     private Chronometer chronometer;
     private TextView recordText;
 
+    private int screenDessity;
+    private MediaRecorder mediaRecorder;
+    private MediaProjectionManager mediaProjectionManager;
+
+    private static final int REQUEST_PERMISSION = 10;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +44,50 @@ public class MainActivity extends AppCompatActivity {
         toggleButton = (ToggleButton)findViewById(R.id.tbSwitch);
         chronometer = (Chronometer)findViewById(R.id.cmTimer);
         recordText = (TextView)findViewById(R.id.tvRecording);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        screenDessity = metrics.densityDpi;
+        mediaRecorder = new MediaRecorder();
+        mediaProjectionManager = (MediaProjectionManager)getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* Check if the Ext Storage Write and Audio Record permissions are granted */
+                if(ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) +
+                   ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+                {
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
+                       ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                            Manifest.permission.RECORD_AUDIO))
+                    {
+                         /* If Ext Storage Write or Audio Record both permissions are not granted,
+                            show the Snackbar to request the permissions */
+                        Snackbar.make(findViewById(android.R.id.content), R.string.permission_text, Snackbar.LENGTH_INDEFINITE).setAction("ENABLE", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, REQUEST_PERMISSION);
+                            }
+                        }).show();
+
+                        toggleButton.setChecked(false);
+                    }
+                    else {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, REQUEST_PERMISSION);
+                    }
+                }
+                else{
+
+                }
+            }
+        });
     }
 }
